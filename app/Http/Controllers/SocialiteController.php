86842f95df81181f8f8438e663ac42b8c1d24c1f
Zeeta:) <189728939+zetFF@users.noreply.github.com>
2025-04-20 12:36:42 +0700

    commit

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -21,17 +21,30 @@ class SocialiteController extends Controller
         try {
             $googleUser = Socialite::driver('google')->user();
             
-            // Check if user exists
-            $user = User::where('email', $googleUser->getEmail())->first();
+            // Check if user exists by google_id
+            $user = User::where('google_id', $googleUser->getId())->first();
             
+            // If not found by google_id, check by email
             if (!$user) {
-                // Create a new user
-                $user = User::create([
-                    'name' => $googleUser->getName(),
-                    'email' => $googleUser->getEmail(),
-                    'password' => Hash::make(Str::random(24)),
-                    'email_verified_at' => now(),
-                ]);
+                $user = User::where('email', $googleUser->getEmail())->first();
+                
+                if ($user) {
+                    // Update existing user with Google info
+                    $user->update([
+                        'google_id' => $googleUser->getId(),
+                        'avatar' => $googleUser->getAvatar(),
+                    ]);
+                } else {
+                    // Create a new user
+                    $user = User::create([
+                        'name' => $googleUser->getName(),
+                        'email' => $googleUser->getEmail(),
+                        'google_id' => $googleUser->getId(),
+                        'avatar' => $googleUser->getAvatar(),
+                        'password' => Hash::make(Str::random(24)),
+                        'email_verified_at' => now(),
+                    ]);
+                }
             }
             
             // Login the user
